0d365fae17ddc75c4211a4bc6b2ee49a7d71db37
Gerardo Rodarte <gerardo.rodarte.013@gmail.com>
2018-04-25 11:34:11 -0700

    video 4.4 terminado

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,10 +6,12 @@ import {
   MatButtonModule,
   MatIconModule,
   MatExpansionModule,
-  MatInputModule
+  MatInputModule,
+  MatListModule
 } from '@angular/material';
 import { MaterialButtonComponent } from './material-button/material-button.component';
 import { MaterialInputComponent } from './material-input/material-input.component';
+import { MaterialListComponent } from './material-list/material-list.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -17,15 +19,17 @@ import { MaterialInputComponent } from './material-input/material-input.componen
     MatButtonModule,
     MatInputModule,
     MatExpansionModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatExpansionModule,
-    MatInputModule
+    MatInputModule,
+    MatListModule
   ],
-  declarations: [MaterialDemoComponent, MaterialButtonComponent, MaterialInputComponent]
+  declarations: [MaterialDemoComponent, MaterialButtonComponent, MaterialInputComponent, MaterialListComponent]
 })
 export class MaterialModule { }
