bf4fd2bafffdb5f752c7a78f079060fb4341680e
Gerardo Rodarte <gerardo.rodarte.013@gmail.com>
2018-04-25 11:47:52 -0700

    video 4.5 terminado

--- src/app/material/material-list/material-list.component.ts
+++ src/app/material/material-list/material-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Estudiante } from '../../shared/model/estudiante';
 @Component({
   selector: 'ed-material-list',
   templateUrl: './material-list.component.html',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaterialListComponent implements OnInit {
 
+  estudiante1: Estudiante;
+  estudiante2: Estudiante;
+  estudiante3: Estudiante;
+
   constructor() { }
 
   ngOnInit() {
+    console.log('Inicializando el componente MaterialList');
+    this.estudiante1 = {
+      id: 1,
+      nombre: 'Luis Aviles',
+      ciudad: 'Cochabamba',
+      fotoURL: 'https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg',
+    };
+
+    this.estudiante2 = {
+      id: 2,
+      nombre: 'Jorge Perez',
+      ciudad: 'Lima'
+    };
+
+    this.estudiante3 = {
+      id: 3,
+      nombre: 'Maria Rodriguez',
+      ciudad: 'Bogota'
+    };
   }
 
 }
