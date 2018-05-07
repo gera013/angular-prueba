23f639a0ac392628f6deabc320f4e88d32c820a3
Gerardo Rodarte <gerardo.rodarte.013@gmail.com>
2018-05-07 12:22:52 -0700

    video 6.1 terminado

---
+++ src/app/shared/model/apod.ts
@@ -0,0 +1,10 @@
+export interface Apod {
+    tittle: string;
+    date: string;
+    explanation: string;
+    url: string;
+    hdurl: string;
+    media_type: string;
+    service_version: string;
+
+}
