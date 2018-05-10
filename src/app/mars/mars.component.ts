a0e0cc03a983f9848d7d408822d27953de6ad798
Gerardo Rodarte <gerardo.rodarte.013@gmail.com>
2018-05-10 12:10:58 -0700

    video 8.3 terminado

--- src/app/mars/mars.component.ts
+++ src/app/mars/mars.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MarsImage } from '../shared/model/mars-image';
+import { NasaApiService } from '../shared/services/nasa-api.service';
 @Component({
   selector: 'ed-mars',
   templateUrl: './mars.component.html',
   styleUrls: ['./mars.component.css']
 })
 export class MarsComponent implements OnInit {
+  marsImages: MarsImage[];
 
-  constructor() { }
+  // Inyeccion de dependecias
+  constructor(private servicio: NasaApiService) { }
 
   ngOnInit() {
+    this.servicio.getMarsImages('CHEMCAM')
+    .subscribe(result => {
+      this.marsImages = result.photos;
+    });
   }
 
 }
--- src/app/shared/services/nasa-api.service.ts
+++ src/app/shared/services/nasa-api.service.ts
@@ -4,7 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 const APOD_URL = 'https://api.nasa.gov/planetary/apod';
-const API_KEY = 'DEMO_KEY';
+const MARS_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000';
+const API_KEY = 'DSqKAXJAiiea5b2ANXJ9ulzzBmDsuVnkO9qIen9a';
 
 @Injectable()
 export class NasaApiService {
@@ -17,6 +18,10 @@ export class NasaApiService {
     // return this.httpClient.get<Apod>(APOD_URL + '?api_key=' + API_KEY);
     // template literals
     return this.httpClient.get<Apod>(`${APOD_URL}?api_key=${API_KEY}`);
+  }
+
+  getMarsImages(camara: string): Observable<any> {
+  return this.httpClient.get(`${MARS_URL}&camara=${camara}&api_key=${API_KEY}`);
   }
 
 }
